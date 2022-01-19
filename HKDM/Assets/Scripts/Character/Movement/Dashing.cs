615688ec08256d46b83c4ccc8055a099a4e4955b
IsaaColomer <60975028+IsaaColomer@users.noreply.github.com>
2022-01-19 08:19:41 +0100

    dash

---
+++ HKDM/Assets/Scripts/Character/Movement/Dashing.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dashing : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rb;
+    public Transform bacK;
+    public float explotionRadius = 1f;
+    public float force = 1000;
+    public Camera camera;
+    public float timeToDash = 2f;
+    private float timeToDash2;
+    private bool dashed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeToDash2 = timeToDash;
+        rb = GetComponent<Rigidbody>();
+        dashed =false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Q) && !dashed)
+        {
+            rb.AddExplosionForce(force, bacK.position, explotionRadius);
+            dashed = true;
+            Debug.Log("Dash");
+        }
+        if(dashed)
+        {
+            if(timeToDash>= 0)
+            {
+                timeToDash-= Time.deltaTime;
+            }
+            else
+            {
+                timeToDash = timeToDash2;
+                dashed = false;                
+            }
+        }
+    }
+}
