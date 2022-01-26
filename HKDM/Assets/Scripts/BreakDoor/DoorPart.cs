99b8f95d435f93b128aaf5a507ebef0e37b97c59
IsaaColomer <60975028+IsaaColomer@users.noreply.github.com>
2022-01-26 20:18:59 +0100

    now we break skeleton

--- HKDM/Assets/Scripts/BreakDoor/DoorPart.cs
+++ HKDM/Assets/Scripts/BreakDoor/DoorPart.cs
@@ -5,17 +5,24 @@ using UnityEngine;
 public class DoorPart : MonoBehaviour
 {
     private Rigidbody rb;
+    public float timer;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        Debug.Log("Adeu");
         rb.AddExplosionForce(5000f, transform.position, 1f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(timer >= 0)
+        {
+            timer -= Time.deltaTime;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
