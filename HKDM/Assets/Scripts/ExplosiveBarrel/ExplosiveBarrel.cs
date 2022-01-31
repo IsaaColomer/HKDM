37a8186a703349c969ba1144c9a145da0f07d42c
IsaaColomer <60975028+IsaaColomer@users.noreply.github.com>
2022-01-31 09:53:57 +0100

    added explosion force to the barrels

--- HKDM/Assets/Scripts/ExplosiveBarrel/ExplosiveBarrel.cs
+++ HKDM/Assets/Scripts/ExplosiveBarrel/ExplosiveBarrel.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class ExplosiveBarrel : MonoBehaviour
 {
+    private Rigidbody rb;
+    [Range(0,10000f)] public float force;
+    [Range(0,50f)] public float range;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "PlayerBullet")
+        {
+            rb.constraints = RigidbodyConstraints.None;
+            rb.AddExplosionForce(force,other.ClosestPoint(transform.position),range);
+        }
     }
 }
